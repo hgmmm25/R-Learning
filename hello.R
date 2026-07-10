5c5cb09b20b3cca8cb73edf000d0b588b2e794ad
HuGe <hgmmmm254@gmail.com>
2026-07-10 14:38:36 +0800

    注釋與嵌套函數的使用

--- hello.R
+++ hello.R
@@ -1,5 +1,9 @@
+#詢問用戶姓名
 a <- readline("What's your name?")
+#正常函數
 Greeting <- paste("Hello, ", a, sep = "")
 Comment1 <- paste0(a, " is a good person.")
 print(Greeting)
 print(Comment1)
+#嵌套函數
+print(paste0("Bye, ", a))
